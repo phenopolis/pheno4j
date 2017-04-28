f7a40d9eecaf77e214b21b84f02e7a100526fe90
Sajid Mughal <sajidmughal@msn.com>
2017-04-28 21:28:21 +0100

    optimise the class that filters the json file for specific variants

--- src/main/java/com/graph/db/file/annotation/AnnotationFileFilter.java
+++ src/main/java/com/graph/db/file/annotation/AnnotationFileFilter.java
@@ -32,6 +32,7 @@ import com.graph.db.util.ManagedEventBus.PoisonPill;
 public class AnnotationFileFilter extends AbstractParser {
 	
 	private final String annotationFileName;
+	private final Gson gson;
 	
 	public AnnotationFileFilter() {
 		this(config.getString("annotationParser.input.fileName"));
@@ -39,10 +40,18 @@ public class AnnotationFileFilter extends AbstractParser {
 	
 	public AnnotationFileFilter(String annotationFile) {
 		this.annotationFileName = annotationFile;
-		
 		if (StringUtils.isBlank(annotationFile)) {
 			throw new RuntimeException("annotationFileName cannot be empty");
 		}
+		
+		gson = createGson();
+	}
+	
+	private Gson createGson() {
+		GsonBuilder b = new GsonBuilder();
+        b.registerTypeAdapter(GeneticVariant.class, new CustomJsonDeserializer());
+        b.setLenient();
+        return b.create();
 	}
 	
 	@Override
@@ -52,12 +61,38 @@ public class AnnotationFileFilter extends AbstractParser {
 
 	@Override
 	protected void processDataForFile(LineNumberReader reader) throws IOException {
-		String line;
+		Set<String> variantIdsToKeep = loadVariantIdsToKeep();
+		LOGGER.info("Searching for {} variant ids", variantIdsToKeep.size());
 		
+		String line;
 		while (( line = reader.readLine()) != null) {
 			logLineNumber(reader, 1000);
-			eventBus.post(line);
+			
+			GeneticVariant geneticVariant = gson.fromJson(line, GeneticVariant.class);
+	    	if (variantIdsToKeep.contains(geneticVariant.getVariant_id())) {
+	    		eventBus.post(line);
+	    	}
 		}
+	}
+
+	private Set<String> loadVariantIdsToKeep() {
+		Set<String> variantIdsToKeep = new HashSet<>();
+		
+		String vcfOutputHetFile = config.getString("annotationFileFilter.input.vcfOutputHetFile");
+		String vcfOutputHomFile = config.getString("annotationFileFilter.input.vcfOutputHomFile");
+		
+		variantIdsToKeep.addAll(extractVariantsFromFile(vcfOutputHetFile));
+		variantIdsToKeep.addAll(extractVariantsFromFile(vcfOutputHomFile));
+		return variantIdsToKeep;
+	}
+	
+	private Set<String> extractVariantsFromFile(String file) {
+		List<String> lines = FileUtil.getLines(file);
+		return lines.stream()
+			.map(line -> StringUtils.split(line, COMMA))
+			.map(fields -> fields[0])
+			.map(variantId -> StringUtils.replace(variantId, DOUBLE_QUOTE, ""))
+			.collect(toSet());
 	}
 
 	@Override
@@ -71,21 +106,9 @@ public class AnnotationFileFilter extends AbstractParser {
 	}
 	
 	private class FilteredAnnotationSubscriber implements AutoCloseable {
-		private final Set<String> variantIds;
-		private final Gson gson;
 		private final BufferedWriter writer;
 		
 		public FilteredAnnotationSubscriber() {
-			String vcfOutputHetFile = config.getString("annotationFileFilter.input.vcfOutputHetFile");
-			String vcfOutputHomFile = config.getString("annotationFileFilter.input.vcfOutputHomFile");
-					
-			variantIds = new HashSet<>();
-			variantIds.addAll(extractVariantsFromFile(vcfOutputHetFile));
-			variantIds.addAll(extractVariantsFromFile(vcfOutputHomFile));
-			LOGGER.info("Searching for {} variant ids", variantIds.size());
-			
-			gson = createGson();
-			
 			File fout = new File(getOutputFolder() + File.separator + "test.json");
 			FileOutputStream fos;
 			try {
@@ -96,22 +119,6 @@ public class AnnotationFileFilter extends AbstractParser {
 			writer = new BufferedWriter(new OutputStreamWriter(fos));
 		}
 
-		private Set<String> extractVariantsFromFile(String file) {
-			List<String> lines = FileUtil.getLines(file);
-			return lines.stream()
-				.map(line -> StringUtils.split(line, COMMA))
-				.map(fields -> fields[0])
-				.map(variantId -> StringUtils.replace(variantId, DOUBLE_QUOTE, ""))
-				.collect(toSet());
-		}
-		
-		private Gson createGson() {
-			GsonBuilder b = new GsonBuilder();
-	        b.registerTypeAdapter(GeneticVariant.class, new CustomJsonDeserializer());
-	        b.setLenient();
-	        return b.create();
-		}
-		
 		@Subscribe
 	    public void processRow(Object line) {
 	    	if (line instanceof PoisonPill) {
@@ -119,18 +126,12 @@ public class AnnotationFileFilter extends AbstractParser {
 	    	}
 	    	
 	    	String json = (String) line;
-			GeneticVariant geneticVariant = gson.fromJson(json, GeneticVariant.class);
-	    	String variant_id = geneticVariant.getVariant_id();
-	    	
-	    	if (variantIds.contains(variant_id)) {
-	    		try {
-	    			LOGGER.info("found: {}", variant_id);
-	    			writer.write(json);
-	    			writer.newLine();
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-	    	}
+    		try {
+    			writer.write(json);
+    			writer.newLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
 	    }
 
 		@Override
