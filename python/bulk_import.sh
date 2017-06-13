c10ed7d05813ccf5a94b0cde03bab2658eb33aec
pontikos <npontikos@gmail.com>
2017-06-13 12:19:42 +0100

    bulk_import

--- python/bulk_import.sh
+++ python/bulk_import.sh
@@ -9,7 +9,7 @@ sort -u -o import/TranscriptVariant.csv import/TranscriptVariant.csv
 
 rm -f data/databases/graph.db/*
 
-./bin/neo4j-admin import --database graph.db \
+./bin/neo4j-import --into graph.db \
 --id-type string \
 --nodes:Transcript import/Transcript-header.csv,import/Transcript.csv \
 --nodes:TranscriptVariant import/TranscriptVariant-header.csv,import/TranscriptVariant.csv \
@@ -20,5 +20,7 @@ rm -f data/databases/graph.db/*
 --relationships:GeneticVariantToTranscriptVariant import/GeneticVariantToTranscriptVariant.csv \
 --relationships:TranscriptToGene import/TranscriptToGene.csv \
 --relationships:HomVariantToPerson import/HomVariantToPerson.csv \
---relationships:HetVariantToPerson import/HetVariantToPerson.csv 
+--relationships:HetVariantToPerson import/HetVariantToPerson.csv \
+--bad-tolerance 1000000 \
+--skip-bad-relationships true
 
